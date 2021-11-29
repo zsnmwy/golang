1982e7105b7819ca19febda940faf77090f7b1b3
zsnmwy <szlszl35622@gmail.com>
2021-11-29 16:47:35 +0800

    Log Level && Accept Env To Control Log

--- homework/httpserver/main.go
+++ homework/httpserver/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	flag "flag"
 	"io"
-	"log"
 	"net/http"
 	"net/http/pprof"
 	_ "net/http/pprof"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/golang/glog"
@@ -17,6 +19,7 @@ import (
 const (
 	ListenAddr  = ":80"
 	NormalLevel = 2
+	DebugLevel  = 4
 )
 
 // 1.接收客户端 request，并将 request 中带的 header 写入 response header
@@ -24,6 +27,17 @@ const (
 // 3.Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 // 4.当访问 localhost/healthz 时，应返回200
 func main() {
+
+	// 接收外部环境变量控制日志级别
+	if os.Getenv("Debug") == "true" {
+		flag.Set("v", "4")
+	} else {
+		flag.Set("v", "2")
+	}
+
+	// 默认就让日志输出在std，应用本身不负责日志采集
+	flag.Set("logtostderr", "true")
+	flag.Parse()
 
 	glog.V(NormalLevel).Info("Starting http server...")
 
@@ -36,11 +50,34 @@ func main() {
 		listenAddr = ListenAddr
 	}
 
-	// Boot web application
-	err := http.ListenAndServe(listenAddr, mux)
-	if err != nil {
-		log.Fatal(err)
+	// Web Instance
+	service := http.Server{
+		Addr:    listenAddr,
+		Handler: mux,
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := service.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			glog.Fatalf("listen: %s\n", err)
+		}
+	}()
+	glog.V(NormalLevel).Info("Server Started")
+
+	<-done
+
+	glog.V(NormalLevel).Info("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := service.Shutdown(ctx); err != nil {
+		glog.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	glog.V(NormalLevel).Info("Server Exited Properly")
 
 }
 
@@ -67,7 +104,7 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = io.WriteString(w, "ok")
 	t := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Printf("time: %v, health check \n", t)
+	glog.V(DebugLevel).Info("time: %v, health check \n", t)
 }
 
 // rootHandler Path /
@@ -75,12 +112,13 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	// copy header form request
 	for k, v := range r.Header {
 		w.Header().Add(k, strings.Join(v, ","))
-		fmt.Println(k, v)
+		glog.V(DebugLevel).Info(k, v)
 	}
 	vEnv := os.Getenv("VERSION")
 	if vEnv == "" {
 		vEnv = "0.0.0"
 	}
+	glog.V(DebugLevel).Info("vEnv: ", vEnv)
 	w.Header().Add("VERSION", vEnv)
 	w.WriteHeader(http.StatusOK)
 	logResponse(http.StatusOK, r)
@@ -89,5 +127,5 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 // print remote client address , port , status code
 func logResponse(statusCode int, r *http.Request) {
 	t := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Printf("time: %s, remote client: %s, status code: %d\n", t, r.RemoteAddr, statusCode)
+	glog.V(DebugLevel).Infof("time: %s, remote client: %s, status code: %d\n", t, r.RemoteAddr, statusCode)
 }
